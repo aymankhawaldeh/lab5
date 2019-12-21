1ca3ee337c346e72a11f93c3e638139cdf7ed88b
Ayman <aymankhawaldeh8@gmail.com>
2019-12-21 15:14:58 +0200

    finally

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 5509;
+server.use(express, static("public"));
+
 server.get('/test', (require, response) =>{
 
     response.send("work");
