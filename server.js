a82a40db15697e60dd9b62bbd9ae7168d48c3dfc
Ayman <aymankhawaldeh8@gmail.com>
2019-12-21 14:32:30 +0200

    server is working now

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const server = express();
+const PORT = process.env.PORT || 5509;
+server.get('/test', (require, response) =>{
+
+    response.send("work");
+});
+
+
+
+server.listen(PORT, () => console.log("listen on port 5509"));
